b3a6e4ee829d7742ae0aeb46dd2480f72d57e551
fernandoZ <fernandozwetsch@gmail.com>
2021-10-19 12:52:22 -0300

    mock login route

--- src/routes.js
+++ src/routes.js
@@ -8,4 +8,29 @@ router.put('/user', user.update);
 router.get('/user/:id', user.show);
 router.delete('/user/:id', user.remove);
 
+//login
+router.post('/login', function (req, res) {
+    const { email, password} = req.body;
+   
+    if (!email) {
+      res.status(400).json({
+        success: false,
+        message: 'Email is required'
+      });
+    }
+
+    if (!password) {
+      res.status(400).json({
+        success: false,
+        message: 'Password is required'
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      token: 'FB8CADBEFC2E65A9BB3C11311D823EB375564BD171F3E62C3685BBFB8353663D6AB3AD0FB45A56DBAB191AF78BFC14D0B175B15B35D3120DE1D0A417AB42B17B'
+    });
+});
+
+
 module.exports = router;
